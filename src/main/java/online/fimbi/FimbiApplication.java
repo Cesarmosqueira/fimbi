c086b2941eeb109ef31ef39fc1d34f437a10d18a
cesarmosqueira <cesarmanuel.2802@gmail.com>
2022-05-16 22:56:36 -0500

    spring init

---
+++ src/main/java/online/fimbi/FimbiApplication.java
@@ -0,0 +1,13 @@
+package online.fimbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FimbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FimbiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/fimbi/FimbiApplicationTests.java
@@ -0,0 +1,13 @@
+package online.fimbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FimbiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
