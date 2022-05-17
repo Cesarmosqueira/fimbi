0409b1d4460e25496e041c841a8464fae0281db0
cesarmosqueira <cesarmanuel.2802@gmail.com>
2022-05-17 17:45:37 -0500

    idk idr idc

--- api/src/main/java/online/fimbi/Controllers/Test.java
+++ api/src/main/java/online/fimbi/Controllers/Test.java
@@ -2,12 +2,14 @@ package online.fimbi.Controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import online.fimbi.Dto.Bond;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("test")
 public class Test {
