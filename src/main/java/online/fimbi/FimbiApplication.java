2d049d1176fc3a470d3b2e09464fc8384789438a
cesarmosqueira <cesarmanuel.2802@gmail.com>
2022-05-17 00:06:37 -0500

    db set

--- src/main/java/online/fimbi/FimbiApplication.java
+++ src/main/java/online/fimbi/FimbiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FimbiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FimbiApplication.class, args);
 	}
-
 }
